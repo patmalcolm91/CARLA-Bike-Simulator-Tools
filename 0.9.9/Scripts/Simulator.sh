6f99e6afa95cf53a0c59c1489a31c24f3cb92d63
patmalcolm91 <patmalcolm91@gmail.com>
2020-10-01 11:40:05 +0200

    remove redundant AdditionalDisplay scripts

--- 0.9.9/Scripts/Simulator.sh
+++ 0.9.9/Scripts/Simulator.sh
@@ -3,7 +3,7 @@ xrandr --output DP-1 --auto --mode 1920x1080 --pos 5760x0 --rotate normal
 xrandr --output DP-5 --auto --mode 1920x1080 --pos 1920x0 --rotate normal
 xrandr --output DP-3 --auto --mode 1920x1080 --pos 3840x0 --rotate normal
 
-python manual_control_simulator.py --refresh=20 & 
-python AdditionalDisplay_Left.py --sleep=10 --refresh=20 &
-python AdditionalDisplay_Right.py --sleep=10 --refresh=20 &
-python AdditionalDisplay_Rear.py --sleep=10 --refresh=20
+python manual_control_simulator.py --config display_config.yaml --display_name center &
+python AdditionalDisplay.py --config display_config.yaml --display_name left --sleep=5 &
+python AdditionalDisplay.py --config display_config.yaml --display_name right --sleep=5 &
+python AdditionalDisplay.py --config display_config.yaml --display_name left_rear --sleep=5
