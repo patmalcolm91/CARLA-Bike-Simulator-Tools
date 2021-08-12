f27b2bf591ee418c0420e8252bed478c6f8bc211
patmalcolm91 <patmalcolm91@gmail.com>
2021-08-12 10:42:35 +0200

    add tls sync to acs2 scenario and reduce display resolutions

--- 0.9.9/Scripts/Simulator.sh
+++ 0.9.9/Scripts/Simulator.sh
@@ -3,13 +3,16 @@
 # xrandr --output DP-5 --auto --mode 1920x1080 --pos 1920x0 --rotate normal
 # xrandr --output DP-3 --auto --mode 1920x1080 --pos 3840x0 --rotate normal
 
-python manual_control_simulator.py --config display_config.yaml --display_name center --scenario_config acs2_scenario_config.yaml &
+python manual_control_simulator.py --config display_config_acs2.yaml --display_name center --scenario_config acs2_scenario_config.yaml &
 python AdditionalDisplay.py --config display_config.yaml --display_name left --sleep=5 &
 python AdditionalDisplay.py --config display_config.yaml --display_name right --sleep=5 &
 python AdditionalDisplay.py --config display_config.yaml --display_name left_rear --sleep=5 &
 
 cd /home/tum-vt/Desktop/carla-dev/Co-Simulation/Sumo
-python run_synchronization.py --sumo-gui --sync-vehicle-lights /home/tum-vt/CARLA-Bike-Simulator-Tools/0.9.9/Scripts/AtCityStudy2/AtCity_Study2.sumocfg &
+python run_synchronization.py --sumo-gui --sumo-port 8813 --sync-vehicle-lights /home/tum-vt/CARLA-Bike-Simulator-Tools/0.9.9/Scripts/AtCityStudy2/AtCity_Study2.sumocfg &
+
+cd /home/tum-vt/CARLA-Bike-Simulator-Tools/0.9.9/Scripts &&
+python TLSSync.py --net "/home/tum-vt/CARLA-Bike-Simulator-Tools/0.9.9/Scripts/AtCityStudy2/AtCity_Study2.net.xml" --config "acs2-tls.yaml" --run &
 
 cd /home/tum-vt/CARLA-Bike-Simulator-Tools/0.9.9/Scripts/AtCityStudy2
 python AtCity_Study2.py &
